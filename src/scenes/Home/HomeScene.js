dc3d2d51244c36785ee472acc0d326bf854923fa
Adrian <yohanesadrian97@gmail.com>
2018-12-08 19:39:49 +0700

    ganti text di homescene

--- src/scenes/Home/HomeScene.js
+++ src/scenes/Home/HomeScene.js
@@ -64,7 +64,7 @@ export default class HomeScene extends Component<Props, State> {
             alignSelf: 'stretch',
           }}
         >
-          <Text>{`${param} ganteng`}</Text>
+          <Text>{`${param} ganteng bnget deh`}</Text>
           <Text>{`${pass} ds`}</Text>
         </View>
       </View>
